839235e76bd01c338c3ff547e14cffb616068e4c
elianaakim <140842030+elianaakim@users.noreply.github.com>
2025-04-09 21:54:12 -0700

    update to wear puffer when temperature is less than 25 and to wear a heavy coat if less than 45

--- ElianaKimKwk3.playground/Contents.swift
+++ ElianaKimKwk3.playground/Contents.swift
@@ -1,9 +1,9 @@
 var temperatureInF = 32
 
 if temperatureInF <= 25{
-    print("You should put on a heavy coat")
-} else if temperatureInF <= 65 {
-    print("We recommend wearing a jacket")
+    print("You should put on a puffer")
+} else if temperatureInF <= 45 {
+    print("We recommend wearing a heavy coat")
 } else {
     print("Your outfit should be good as is")
 }
